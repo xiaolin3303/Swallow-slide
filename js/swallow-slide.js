9638820c473a7b2ee65b71d285c308e87b70259a
fang <xiaolin3303@163.com>
2014-07-29 14:22:00 +0800

    V1.0
    first commit

---
+++ js/swallow-slide.js
@@ -0,0 +1,49 @@
+function move(direction) {
+	var currentEle = $('#stage .box .current');
+
+	direction = direction || 'right';
+
+	if(direction === 'right') {
+		currentEle.removeClass('current').addClass('left-side');
+		currentEle.next().removeClass('right-side')
+					.addClass('current');
+		currentEle = currentEle.next();
+	}else{
+		currentEle.removeClass('current').addClass('right-side');
+		currentEle.prev().removeClass('left-side')
+					.addClass('current');
+		currentEle = currentEle.prev();
+	}
+
+	//更新各元素的left值
+	var	prevAll = currentEle.prevAll(),
+		nextAll = currentEle.nextAll();
+	currentEle.css({'left': 0, 'z-index': ''});
+	prevAll.each(function(index) {
+		var _offset = [-150, -200, -250];
+		if(index < _offset.length) {
+			$(this).removeClass('hide').css('left', _offset[index]);
+		}else{
+			$(this).addClass('hide');
+		}
+	});
+	nextAll.each(function(index) {
+		var _offset = [150, 200, 250];
+		if(index < _offset.length) {
+			$(this).removeClass('hide').css({'left': _offset[index], 'z-index': (3-index)});
+		}else{
+			$(this).addClass('hide');
+		}
+	});
+
+}
+
+$(function() {
+	$('#stage .box > li').on('click', function() {
+		if($(this).hasClass('left-side')) {
+			move('left');
+		}else if($(this).hasClass('right-side')){
+			move('right');
+		}
+	});
+});
